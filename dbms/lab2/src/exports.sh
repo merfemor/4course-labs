e35e26b102c0d17a9ade434a066ce8e7e87b693b
Roman Efremov <merfemor@yandex-team.ru>
2019-11-25 16:42:27 +0300

    dbms lab2 fix path

--- dbms/lab2/src/exports.sh
+++ dbms/lab2/src/exports.sh
@@ -1,6 +1,6 @@
 export ORACLE_SID=s225111
 export ORACLE_BASE=/u01/app/oracle
-export ORACLE_HOME=$ORACLE_BASE/product/11.2.0/dbhome_1
+export ORACLE_HOME=$ORACLE_BASE/product/11.2.0/dbhome_1/
 export PATH="$PATH:${ORACLE_HOME}/bin"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${ORACLE_HOME}/lib"
 export NLS_LANG=American_America.UTF8
