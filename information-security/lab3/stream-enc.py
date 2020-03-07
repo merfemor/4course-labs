b15b3665f8955732d9701461ac2ca2203067e4d7
Roman Efremov <merfemor@yandex-team.ru>
2020-03-07 23:24:36 +0300

    infsec: lab3: add comments

--- information-security/lab3/stream-enc.py
+++ information-security/lab3/stream-enc.py
@@ -6,23 +6,29 @@ import getpass
 import sys
 import bitarray
 
-LRS1_SIZE = 78
-LRS2_SIZE = 54
-LRS1_COEFF = [77, 6, 5, 2, 0]
-LRS2_COEFF = [53, 6, 2, 1, 0]
+LFSR1_SIZE = 78
+LFSR2_SIZE = 54
+LFSR1_COEFF = [77, 6, 5, 2, 0]
+LFSR2_COEFF = [53, 6, 2, 1, 0]
 
 
 def string_to_bits(string):
+    """Преобразует строку в массив бит
+    """
     ba = bitarray.bitarray()
     ba.frombytes(string.encode("ascii"))
     return ba.tolist()
 
 
 def bits_to_byte(bits):
+    """Преобразует массив из 8 бит в байт
+    """
     return bitarray.bitarray(bits).tobytes()[0]
 
 
 def key_string_to_bits(key_string, array_size):
+    """Преобразует строку ключа, введенную пользователем в массив бит заданной длины.
+    """
     string_bits = string_to_bits(key_string)
     if len(string_bits) < array_size:
         num_s = (array_size - len(string_bits)) // len(string_bits)
@@ -30,33 +36,40 @@ def key_string_to_bits(key_string, array_size):
     return string_bits[:array_size]
 
 
-def modify_lrs_state(lrs_state, lrs_coeff):
+def modify_lsfr_state(lsfr_state, lsfr_coeff):
+    """Возвращает измененнное состояние регистра после одного такта.
+    Биты в регистре переносятся вправо, а в последний бит записывается XOR битов,
+    указанных в массиве коэффициентов lsfr_coeff.
+    """
     last_el = False
-    for co in lrs_coeff:
-        last_el ^= lrs_state[co]
-    return lrs_state[1:] + [last_el]
+    for co in lsfr_coeff:
+        last_el ^= lsfr_state[co]
+    return lsfr_state[1:] + [last_el]
 
 
 def gamma_generator(key_string):
-    """Генератор гамма-последовательности. Возвращает по биту.
+    """Генератор гамма-последовательности. Возвращает очередной бит последовательности.
+    Моделирует работу регистров потактово.
     """
-    lrs1_state = key_string_to_bits(key_string, LRS1_SIZE)
-    lrs2_state = key_string_to_bits(key_string, LRS2_SIZE)
+    lsfr1_state = key_string_to_bits(key_string, LFSR1_SIZE)
+    lsfr2_state = key_string_to_bits(key_string, LFSR2_SIZE)
     t = 1
 
     while True:
-        first_and = lrs1_state[77] and lrs2_state[52]
-        second_and = lrs1_state[3] and lrs2_state[41]
+        first_and = lsfr1_state[77] and lsfr2_state[52]
+        second_and = lsfr1_state[3] and lsfr2_state[41]
 
         yield first_and ^ second_and
 
-        lrs1_state = modify_lrs_state(lrs1_state, LRS1_COEFF)
+        lsfr1_state = modify_lsfr_state(lsfr1_state, LFSR1_COEFF)
         if t % 3 == 0:
-            lrs2_state = modify_lrs_state(lrs2_state, LRS2_COEFF)
+            lsfr2_state = modify_lsfr_state(lsfr2_state, LFSR2_COEFF)
         t += 1
 
 
 def gamma_generator_bytes(key_string):
+    """То же, что и gamma_generator, но возвращает байты, выбирая сразу 8 значений из gamma_generator.
+    """
     gen = gamma_generator(key_string)
     while True:
         bits = [next(gen) for _ in range(8)]
@@ -64,6 +77,11 @@ def gamma_generator_bytes(key_string):
 
 
 def read_and_perform_encrypt_or_decrypt(password):
+    """Общая процедура для шифрования и дешифрации.
+    Процесс и шифрования, и дешифрации представляет собой XOR входного текста с гамма-последовательностью.
+    Процедура побайтово считывает клавиатурный ввод (stdin), делает XOR с очередным байтом гамма-последовательности,
+    и выводит байт в stdout.
+    """
     generator = gamma_generator_bytes(password)
 
     while True:
